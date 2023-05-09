0419feb0ba3c8148ee33edd8a244b13b93168694
Luca Calcaterra <calcaterra.luca@gmail.com>
2023-05-09 19:31:17 +0200

    tmp

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  typescript: {
+    shim: false,
+  },
+  css: [
+    "primevue/resources/themes/saga-blue/theme.css",
+    "primevue/resources/primevue.css",
+    "primeicons/primeicons.css",
+    "primeflex/primeflex.css",
+  ],
+  build: {
+    transpile: ["primevue"],
+  },
+  imports: {
+    autoImport: true,
+    addons: {
+      vueTemplate: true,
+    },
+  },
+});
