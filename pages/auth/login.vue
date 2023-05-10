e576ccad608f27cb7cd9b130fa8e7c5afacb0583
Luca Calcaterra <calcaterra.luca@gmail.com>
2023-05-10 19:37:50 +0200

    nope

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,12 +1,41 @@
+<script setup lang="ts">
+import { useToast } from "primevue/usetoast";
+
+const pocketbase = usePocketbase();
+const email = ref<string | null>(null);
+const password = ref<string | null>(null);
+const remeberMe = ref<boolean>(false);
+const isLoading = ref<boolean>(false);
+const toast = useToast();
+
+// Login user
+async function login() {
+    isLoading.value = true;
+    if (email && password) {
+        await pocketbase
+            .collection("users")
+            .authWithPassword(email.value as string, password.value as string)
+            .then((user) => {
+                navigateTo("/");
+            })
+            .catch((error) => {
+                console.log(error);
+                toast.add({
+                    severity: "error",
+                    summary: "Error",
+                    detail: "Invalid email or password",
+                    life: 3000,
+                });
+            });
+    }
+    isLoading.value = false;
+    return;
+}
+</script>
+
 <template>
-    <div class="card">
-        <div class="flex flex-wrap md:justify-content-between justify-content-center card-container">
-            <div class="surface-500 hover:surface-700 text-white font-bold m-2 flex align-items-center justify-content-center"
-                style="min-width: 200px; min-height: 100px">hover:surface-700</div>
-            <div class="bg-cyan-500 hover:bg-cyan-700 text-white font-bold m-2 flex align-items-center justify-content-center"
-                style="min-width: 200px; min-height: 100px">hover:bg-cyan-700</div>
-            <div class="bg-orange-500 hover:bg-orange-700 text-white font-bold m-2 flex align-items-center justify-content-center"
-                style="min-width: 200px; min-height: 100px">hover:bg-orange-700</div>
-        </div>
+    <div class="justify-content-center align-content-center display: flex flex-wrap fill-height mt-8">
+        <InputText v-model="email" placeholder="email" />
+        <InputText v-model="password" placeholder="password" />
     </div>
-</template> 
+</template>
